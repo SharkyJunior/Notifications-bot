5dd49c2c1c8b62ab842d714e7f6223dc707c83f8
SharkyJunior <sharkevich.maxim@gmail.com>
2022-02-22 13:21:30 +0300

    added credits function and fixed some stuff

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 from datetime import datetime
-import re
+import re # oh no
 from telegram import *
 from telegram.ext import *
 import constants # you should create constants.py file in bot directory and input api key and database path as variables there
 from jsonhandler import *
 
 DATABASE_PATH = constants.DATABASE_PATH #path to json file where all data is contained
-TIME_REGEX = re.compile("[0-2]\d[.: ][0-5]\d[, ] ?[0-3]\d[/.][01]\d[/.]\d\d\d\d")
-TITLE, DESCRIPTION, DUETIME = range(3)
-reply_keyboard = [['Create new task', 'Info', 'View all tasks', 'View most urgent task', 'Delete task', 'Credits']]
+TIME_REGEX = re.compile("[0-2]\d[.: ][0-5]\d[, ] ?[0-3]\d[/.][01]\d[/.]\d\d\d\d") # to filter incorrect time to prevent errors
+TITLE, DESCRIPTION, DUETIME = range(3) # Conversation handler states
+#---------Default keyboard----------
+reply_keyboard = [['Create new task', 'My info'], ['View all tasks',  'View most urgent task'], ['Delete task', 'Credits']]
 MAINMENU_KEYBOARD = ReplyKeyboardMarkup(
-            reply_keyboard, one_time_keyboard=True, input_field_placeholder='Press a button or write here to start using bot'
+            reply_keyboard, one_time_keyboard=False, input_field_placeholder='Press a button or write here to start using bot'
         )
+#------End of default keyboard------
 new_task = {}
+is_creating = False
 
 def start(update: Update, context: CallbackContext):
     if(not str(update.message.from_user.id) in LoadJson(DATABASE_PATH).keys()): #checking if user id already in database
@@ -24,40 +27,49 @@ def start(update: Update, context: CallbackContext):
     f"Hello {update.message.from_user.first_name}, I'm notifications bot. I can help you remember all your events and importnat things.",
     reply_markup=MAINMENU_KEYBOARD)
 
-#TODO: make a conversation handler to make new tasks
+#-----Stuff for ConversationHandler-----
 def start_task_creation(update: Update, context: CallbackContext):
+    global is_creating
+    is_creating = True
     update.message.reply_text(f"Ok {update.message.from_user.first_name}, let's make your task! You can cancel creation by sending /cancel.\nSend me task title...",
     reply_markup=ReplyKeyboardRemove())
     return TITLE
 
 def got_title(update: Update, context: CallbackContext):
-    new_task = {"title" : update.message.text}
-    update.message.reply_text(f"Fine, now provide some description for {update.message.text.lower()} (if you're lazy, just type 'no')...")
-    return DESCRIPTION
+    if is_creating:
+        new_task = {"title" : update.message.text}
+        update.message.reply_text(f"Fine, now provide some description for {update.message.text.lower()} (if you're lazy, just type 'no')...")
+        return DESCRIPTION
+    else:
+        print('oh no cringe')
 
 def got_description(update: Update, context: CallbackContext):
-    reply = update.message.text
-    if not reply == 'no':
-        new_task["description"] = update.message.text
-        update.message.reply_text("Great! Now, finally, what is the deadline for your task (MM:HH DD:MM:YYYY)?")
-    else:
-        new_task["description"] = "No description provided."
-        update.message.reply_text("Not surprising. Now, finally, what is the deadline for your task (MM:HH DD:MM:YYYY)?")
-    return DUETIME
+    if is_creating:
+        reply = update.message.text
+        if not reply == 'no':
+            new_task["description"] = update.message.text
+            update.message.reply_text("Great! Now, finally, what is the deadline for your task (MM:HH DD:MM:YYYY)?")
+        else:
+            new_task["description"] = "No description provided."
+            update.message.reply_text("Not surprising. Now, finally, what is the deadline for your task (MM:HH DD:MM:YYYY)?")
+        return DUETIME
 
 #time regex: [0-2]\d[.: ][0-5]\d[, ] ?[0-3]\d[/.][01]\d[/.]\d\d\d\d (12:34 12/12/2020 - matches)
 def got_duetime(update: Update, context: CallbackContext):
-    reply = update.message.text
-    if TIME_REGEX.match(reply):
-        print("regex matched")
-        update.message.reply_text("Done!", reply_markup=MAINMENU_KEYBOARD)
-        return ConversationHandler.END
-    else:
-        update.message.reply_text("Oops! I cannot understand this type of time, please try again.")
-        return DUETIME
-    
+    if is_creating:
+        reply = update.message.text
+        if TIME_REGEX.match(reply):
+            print("regex matched")
+            update.message.reply_text("Done!", reply_markup=MAINMENU_KEYBOARD)
+            return ConversationHandler.END
+        else:
+            update.message.reply_text("Oops! I cannot understand this type of time, please try again.")
+            return DUETIME
+#----------End of this stuff----------
 
 def todo_list(update: Update, context: CallbackContext):
+    global is_creating
+    is_creating = False
     data = LoadJson(DATABASE_PATH)
     id = update.message.from_user.id
     if(str(id) in data.keys()): # checks if user is in database (good to prevent strange errors) 
@@ -69,9 +81,17 @@ def todo_list(update: Update, context: CallbackContext):
     else:
         print("cringe")
 
+#-----ConverstionHandler fallback-----
 def cancel_creation(update: Update, context: CallbackContext):
+    global is_creating
+    is_creating = False
     update.message.reply_text("Ok.", reply_markup=MAINMENU_KEYBOARD)
     return ConversationHandler.END
+
+def credits(update: Update, context: CallbackContext):
+    global is_creating
+    is_creating = False
+    update.message.reply_text(open('credits.txt').read())
 
 def main():
     print("Starting bot...")
@@ -85,11 +105,13 @@ def main():
                             states = {TITLE : [MessageHandler(Filters.text, got_title)],
                                     DESCRIPTION : [MessageHandler(Filters.text, got_description)],
                                     DUETIME : [MessageHandler(Filters.text, got_duetime)]},
-                                    fallbacks = [CommandHandler("cancel", cancel_creation)]) 
+                                    fallbacks = [CommandHandler("cancel", cancel_creation)],
+                                    allow_reentry=True) 
 
     dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(CommandHandler("taskslist", todo_list))
+    dispatcher.add_handler(MessageHandler(Filters.regex("(?i)View all tasks"), todo_list))
     dispatcher.add_handler(MessageHandler(Filters.regex("(?i)cancel|break|stop"), cancel_creation), group=0)
+    dispatcher.add_handler(MessageHandler(Filters.regex("(?i)credits|socials|creators|devs|developers"), credits))
     dispatcher.add_handler(taskcr_conv_handler)
     
     updater.start_polling()
